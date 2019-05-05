50a2e132402f6a01f722656cadc48a153fe261c6
Xi Gong <xi@ucsb.edu>
2019-05-05 15:45:29 -0700

    1

--- HashSet.cpp
+++ HashSet.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 HashSet::HashSet(){
     nitems = 0;
-    nslots = 30; 
+    nslots = 10000; 
     strfn = new PearsonHash(); 
     intfn = new ReciprocalHash(1, nslots);
     slots = new string*[nslots];
