b717ce0af726eac5036af19d8722839026bde79c
Xi Gong <xi@ucsb.edu>
2019-05-05 15:17:15 -0700

    1

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -10,13 +10,12 @@ BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
 	this->k = k; //k = # hashfunctions
 	this->m = m; //m = # bits
 	intfns = new IntegerHash*[k]; 
-//	cout<<"Int hash is: "+ intfn<<endl;
-//	cout << "String hash is" + strfn <<endl; 
+
 	bits = new uint64_t[(int)m/4 + 3]; //how to determine the size of bit array;
 	if(strfn == "jenkins"){
-		this->strfn = new JenkinsHash(); //wrong
+		this->strfn = new JenkinsHash();
 	}else if(strfn == "pearson"){
-		this->strfn = new PearsonHash(); //wrong
+		this->strfn = new PearsonHash(); 
 	}
 	
 	if(intfn == "division"){
@@ -52,14 +51,10 @@ bool BloomFilter::lookup(const std::string& value) const{
 		uint64_t id = (*intfns[i]).hash(v);
 		uint64_t temp = (uint64_t)bits[id/4] & ((uint64_t)1 << (id % 4)); 
 		if(temp != ((uint64_t)1 << (id % 4))){
-//			if(temp != ((uint64_t)1 << (id %4)))
-//				cout<<"false"<<endl;
-//			cout<<(uint64_t)(1 << (id % 4)) <<endl;
-//			cout<< bits[id/4] <<endl;  
+			if(temp != ((uint64_t)1 << (id %4)))  
 			return false;
 		} 
 	}
-//	cout << "true" << endl; 
 	return true; 
 }
 
--- HashSet.cpp
+++ HashSet.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 HashSet::HashSet(){
     nitems = 0;
-    nslots = 100; 
+    nslots = 30; 
     strfn = new PearsonHash(); 
- //   StringHash* strfn2 = new Prob(); //probing function
     intfn = new ReciprocalHash(1, nslots);
     slots = new string*[nslots];
 }
@@ -16,14 +15,15 @@ HashSet::HashSet(){
 void HashSet::rehash(){
     string** newslots = new string*[2*nslots];
     for(int i = 0; i < nslots; i++){
-        if(slots[i] != NULL){
+        if(slots[i]){
             int v = (*strfn).hash(*slots[i]);
 	    string value = *slots[i];  
-            int id = (*intfn).hash(v); 
-            while(newslots[id] != NULL){
+            int id = (*intfn).hash(v);
+            while(newslots[id]){
                 id ++; 
             }
-        *newslots[id] = value; //insertion completed
+	
+        newslots[id] = new string(value); //insertion completed
         }
     }
     slots = newslots; //free pointer 
@@ -35,36 +35,34 @@ void HashSet::insert(const std::string& value){
     //rehash if oversize;  
     if(nitems == nslots) rehash(); 
 
-    int v = (*strfn).hash(value); 
+    int v = (*strfn).hash(value);
     int id = (*intfn).hash(v); 
+ 
     while(slots[id] != NULL){
         id = (id + 1)% nslots; 
     }
-    string* temp = new string(value);  
-    slots[id] = temp; //insertion completed
-    //cout << *slots[id] << endl; 
-    delete temp; 
+
+    slots[id] = new string(value); //insertion completed 
     nitems ++; 
 }
 
 bool HashSet::lookup(const std::string& value)const{
     int v = (*strfn).hash(value); 
-    int id =(*intfn).hash(v); 
-    while(slots[id] != NULL && id < nslots){
-	if(*slots[id] == value)
+    int id =(*intfn).hash(v);  
+    int count = 0; 
+
+    while(count < nslots){
+	if(slots[id] && *slots[id] == value){
             return true;
-	id ++; //linear prob 
+	} 
+	id = (id + 1) % nslots; //linear prob 
+	count ++; 
     }
     return false; //not found
 }
 
 HashSet::~HashSet(){
-//    for(int i = 0; i < nslots; i++){
-//	if(slots[i] != NULL)
-//		cout<<*slots[i]<<endl;
-//    }
     delete strfn; 
-//    delete strfn2;
     delete intfn; 
     delete[] slots; 
 }
